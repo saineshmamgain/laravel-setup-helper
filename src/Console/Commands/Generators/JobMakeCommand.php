a28baf33009ff3d405acdce32ac70c7119288e7c
saineshmamgain <saineshmamgain@gmail.com>
2021-04-02 01:12:02 +0530

    Support for laravel version 6

--- src/Console/Commands/Generators/JobMakeCommand.php
+++ src/Console/Commands/Generators/JobMakeCommand.php
@@ -19,4 +19,17 @@ class JobMakeCommand extends \Illuminate\Foundation\Console\JobMakeCommand
 
         return parent::getStub();
     }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__.$stub;
+    }
 }
