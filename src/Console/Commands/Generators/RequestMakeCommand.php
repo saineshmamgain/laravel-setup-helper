e730c0da542d40af3c91f9fa041289590b83335f
saineshmamgain <saineshmamgain@gmail.com>
2021-04-02 01:22:47 +0530

    Support for laravel version 6

--- src/Console/Commands/Generators/RequestMakeCommand.php
+++ src/Console/Commands/Generators/RequestMakeCommand.php
@@ -12,4 +12,17 @@ class RequestMakeCommand extends \Illuminate\Foundation\Console\RequestMakeComma
 
         return parent::getStub();
     }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__.$stub;
+    }
 }
